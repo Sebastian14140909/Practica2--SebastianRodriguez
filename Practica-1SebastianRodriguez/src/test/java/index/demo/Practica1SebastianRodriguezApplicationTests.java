5264e507bfd397884b3a45beaac51b7db526c8a0
Sebas090909 <tanyr09@gmail.com>
2024-10-08 14:24:15 -0600

    Practica 2
    Practica 2 Sebastian Rodriguez

---
+++ Practica-1SebastianRodriguez/src/test/java/index/demo/Practica1SebastianRodriguezApplicationTests.java
@@ -0,0 +1,13 @@
+package index.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica1SebastianRodriguezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
